fa700b22d5d86778617641b0d6e5a5f22bd99c41
Esma Yıldırım <esmayildirim@gmail.com>
2023-01-06 16:59:26 -0500

    Add files via upload

---
+++ 5_case_study_twitter/keys.py
@@ -0,0 +1,3 @@
+bearer_token = '' # obtained from developer.twitter.com account
+consumer_key='' # obtained from developer.twitter.com account
+consumer_secret='' # obtained from developer.twitter.com account
